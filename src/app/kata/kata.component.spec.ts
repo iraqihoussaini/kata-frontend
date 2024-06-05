dac37f04fc2305dfe96dc9e3c27cc08c3e38fd06
iraqihoussaini <yassineiraqihoussaini@gmail.com>
2024-06-05 09:04:14 +0200

    test: add test for Kata Component

--- src/app/kata/kata.component.spec.ts
+++ src/app/kata/kata.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { KataComponent } from './kata.component';
+import { HeaderComponent } from '../shared/component/header/header.component';
+import { MainComponent } from '../shared/component/main/main.component';
+import { SpinnerComponent } from '../shared/component/spinner/spinner.component';
+import { TemplateOrdredListComponent } from '../shared/component/template-ordred-list/template-ordred-list.component';
+import { TableComponent } from '../shared/component/table/table.component';
+import { ModalFormComponent } from './components/modal-form/modal-form.component';
+import { ModalComponent } from '../shared/component/modal/modal.component';
 
 describe('KataComponent', () => {
   let component: KataComponent;
@@ -8,7 +17,8 @@ describe('KataComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ KataComponent ]
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
+      declarations: [ KataComponent, HeaderComponent, MainComponent, SpinnerComponent, TemplateOrdredListComponent, TableComponent, ModalFormComponent, ModalComponent ], 
     })
     .compileComponents();
 
@@ -19,5 +29,32 @@ describe('KataComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open modal', (done) => {
+    let modalEl = document.querySelector('app-modal') as HTMLElement;
+    expect(modalEl.style.display).toBe('none');
+    component.openModal()
+    fixture.detectChanges();
+    modalEl = document.querySelector('app-modal') as HTMLElement;
+    expect(modalEl.style.display).toBe('block');
+    done();
+  });
+
+  it('should close modal', (done) => {
+    let modalEl = document.querySelector('app-modal') as HTMLElement;
+    expect(modalEl.style.display).toBe('none');
+    component.openModal()
+    fixture.detectChanges();
+    modalEl = document.querySelector('app-modal') as HTMLElement;
+    expect(modalEl.style.display).toBe('block');
+    
+    const btnClose = modalEl = document.querySelector('.btn.btn-secondary') as HTMLElement;
+
+    btnClose.click();
+    fixture.detectChanges();
+    modalEl = document.querySelector('app-modal') as HTMLElement;
+    expect(modalEl.style.display).toBe('none');
+    done();
   });
 });
